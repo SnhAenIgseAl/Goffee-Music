7c9c92d420ad69fe41b5b08f8a3a90ff17b6dfd9
SnhAenIgseAl <103511682+SnhAenIgseAl@users.noreply.github.com>
2024-07-15 20:21:43 +0800

    Update biliTicketCfg.js

--- server/model/biliTicketCfg.js
+++ server/model/biliTicketCfg.js
@@ -1,7 +1,7 @@
 /**
  * 噼哩噼哩的userToken
  */
-const bili_ticket = 'bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjAwMDgyNzAsImlhdCI6MTcxOTc0OTAxMCwicGx0IjotMX0.XGCJBoRNX7QDJAFwktflzlHbv0crTj2Ms1xKucJFjPQ'
+const bili_ticket = 
 
 
 
